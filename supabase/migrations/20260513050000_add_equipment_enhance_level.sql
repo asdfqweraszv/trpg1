d4cd993c76480121dc4792c0591b072bb0fb6896
asdfqweraszv <homosapiansapiansmart@gmail.com>
2026-05-13 15:55:28 +0800

    Updated 20260513050000_add_equipment_enhance_level.sql

--- supabase/migrations/20260513050000_add_equipment_enhance_level.sql
+++ supabase/migrations/20260513050000_add_equipment_enhance_level.sql
@@ -1,4 +1,5 @@
 -- equipment 테이블에 enhance_level 컬럼 추가
 ALTER TABLE equipment ADD COLUMN IF NOT EXISTS enhance_level INTEGER DEFAULT 0;
 
-npx supabase migration up
+SELECT * FROM equipment LIMIT 1;
+
