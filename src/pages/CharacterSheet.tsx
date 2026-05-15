a655247e33c39adf1de729a5caecbcad547f05f7
asdfqweraszv <homosapiansapiansmart@gmail.com>
2026-05-16 00:43:22 +0800

    Updated CharacterSheet.tsx

--- src/pages/CharacterSheet.tsx
+++ src/pages/CharacterSheet.tsx
@@ -318,14 +318,6 @@ const visibleStats = ALL_STATS;
   }
   
   if (loading) {
-    return ( ... );
-  }
-  
-  if (!char) {
-    return ( ... );
-  }
-  
-  if (loading) {
     return (
       <div className="min-h-screen bg-gray-950 flex items-center justify-center">
         <div className="w-8 h-8 border-2 border-blue-500 border-t-transparent rounded-full animate-spin" />
