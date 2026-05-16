a5bd3a3e2f58784f04ee56596c5a088198814a0c
asdfqweraszv <homosapiansapiansmart@gmail.com>
2026-05-16 23:11:39 +0800

    Updated CharacterList.tsx

--- src/pages/CharacterList.tsx
+++ src/pages/CharacterList.tsx
@@ -192,6 +192,7 @@ async function handleCombatEnd() {
     </div>
   ))}
 </div>
+      )}
 
         {showMasterLogin && (
           <div className="fixed inset-0 bg-black/70 flex items-center justify-center z-50 px-4">
