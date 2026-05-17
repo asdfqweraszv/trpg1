2b323273d54d3220664468935fedd1846292c52d
asdfqweraszv <homosapiansapiansmart@gmail.com>
2026-05-17 17:41:47 +0800

    Updated CharacterList.tsx

--- src/pages/CharacterList.tsx
+++ src/pages/CharacterList.tsx
@@ -307,7 +307,6 @@ export default function CharacterList({ onSelect, onCreate, masterMode, setMaste
             </div>
           </div>
         )}
-      </div>  {/* max-w-4xl 닫힘 */}
-    </div>    {/* 배경 div 닫힘 */}
-  );        // 함수 닫힘
-}          // export 닫힘
+      </div>
+  );
+}
